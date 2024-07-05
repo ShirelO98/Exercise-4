53a43f8d9bd02e3615a5ca1ea7f5abafea5c2265
ShirelO98 <shirelo983@gmail.com>
2024-07-05 19:19:26 +0300

    fix the dbConnection and add query

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,12 +1,23 @@
-exports.dbConnection = {
-    async createConnection() {
-        const mysql = require('mysql2/promise');
-        const connection = await mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME
-        });
-        return connection;
-    }
-}
+const createConnection = async () => {
+    const mysql = require("mysql2/promise");
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+    return connection;
+  };
+  
+  const query = async (sql, params) => {
+    const connection = await createConnection();
+    const [results] = await connection.execute(sql, params);
+    connection.end();
+    return results;
+  };
+  
+  module.exports = {
+    createConnection,
+    query,
+  };
+  
