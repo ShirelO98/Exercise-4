3456b0461df68d4ea7d095f5e7fbf93e86e40c52
ShirelO98 <shirelo983@gmail.com>
2024-07-05 19:09:46 +0300

    update the index

--- server/index.js
+++ server/index.js
@@ -1,27 +1,40 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
+const createConnection = require("./dbConnection").createConnection;
+
 // Import routes
-const useres = require('./routers/users');
-// const preferencesRoutes = require('./routers/preferences');
-// const vacationRoutes = require('./routers/vacation');
+const { usersRouter } = require("./routers/users-router");
+const { preferencesRouter } = require("./routers/preferences-router");
+
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.set('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE");
-    res.set('Content-Type', 'application/json');
-    next();
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.set("Content-Type", "application/json");
+  next();
 });
 
 // Routes
-app.use('/users', useres);
-// app.use('/preferences', preferencesRoutes);
-// app.use('/vacation', vacationRoutes);
+app.use("/users", usersRouter);
+app.use("/preferences", preferencesRouter);
 
-app.listen(port);
+app.listen(port, async () => {
+  console.log(`Server is running on port ${port}`);
+  try {
+    const connection = await createConnection();
+    console.log("Connected to the database");
+    connection.end();
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+});
