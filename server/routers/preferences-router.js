5759ac1db56b95eed38c7187d756f93b31bcfbf9
ShirelO98 <shirelo983@gmail.com>
2024-07-09 10:46:12 +0300

    preferences-router

---
+++ server/routers/preferences-router.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const {
+  preferencesController,
+} = require("../controllers/preferencesController");
+
+const preferencesRouter = Router();
+
+preferencesRouter.get("/", preferencesController.//add);
+preferencesRouter.post("/", preferencesController.//add);
+preferencesRouter.put("/:id", preferencesController.//add);
+preferencesRouter.get("/calculate", preferencesController.//add);
+
+module.exports = { preferencesRouter };
